a81b906493e7deab2f09e987624fbdf805eb2329
BAMMBoo <52661037+BAMMBoo@users.noreply.github.com>
2023-12-22 10:43:52 +0800

    Delete Handwriting directory

--- Handwriting/APPs/static/js/image.js
+++
@@ -1,8 +0,0 @@
-function changeImage() {
-        var image = document.getElementById('myImage');
-        if (image.src.endsWith('my_image.jpg')) {
-            image.src = '{% static "images/other_image.jpg" %}';
-        } else {
-            image.src = '{% static "images/my_image.jpg" %}';
-        }
-    }
